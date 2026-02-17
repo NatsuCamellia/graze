e71708ca3694d450058d4526fb66eabc6f393027
NatsuCamellia <natsucamellia@gmail.com>
2026-02-17 12:29:54 +0800

    feat: make cow break wheat crops

---
+++ src/main/java/net/natsucamellia/graze/world/entity/ai/goal/GrazeGoal.java
@@ -0,0 +1,56 @@
+package net.natsucamellia.graze.world.entity.ai.goal;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.PathfinderMob;
+import net.minecraft.world.entity.ai.goal.MoveToBlockGoal;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class GrazeGoal extends MoveToBlockGoal {
+    private final Mob mob;
+
+    @Override
+    public double acceptedDistance() {
+        return 2.0d;
+    }
+
+    public GrazeGoal(PathfinderMob mob, double speedModifier, int searchRange) {
+        super(mob, speedModifier, searchRange, 4);
+        this.mob = mob;
+    }
+
+    @Override
+    protected boolean isValidTarget(LevelReader level, BlockPos pos) {
+        // find mature wheat crop
+        BlockState state = level.getBlockState(pos);
+        return state.is(Blocks.WHEAT) && state.getValue(CropBlock.AGE) >= 7;
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        Level level = this.mob.level();
+        if (!level.isClientSide) {
+            // Look at the crop block
+            this.mob.getLookControl().setLookAt(
+                    (double) this.blockPos.getX() + 0.5D,
+                    this.blockPos.getY(),
+                    (double) this.blockPos.getZ() + 0.5D,
+                    10.0F,
+                    (float) this.mob.getMaxHeadXRot()
+            );
+            if (this.isReachedTarget()) {
+                BlockPos cropPos = this.blockPos;
+                level.destroyBlock(cropPos, false);
+                this.mob.playSound(SoundEvents.CROP_BREAK, 1.0F, 1.0F);
+
+                this.stop();
+            }
+        }
+    }
+}
